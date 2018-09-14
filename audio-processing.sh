c76ae0e95a22795f852fbc6bba0b090242edad8c
Brandon L. Deriso <brandon@Brandons-MacBook-Pro-9.local>
2018-09-14 11:56:36 -0500

    Adding checksum check & temporary file usage to prevent re-running against previously processed files. Simplified some substitution logic as well.

--- audio-processing.sh
+++ audio-processing.sh
@@ -4,7 +4,7 @@
 #set -e
 
 # Debug Mode
-#set -x
+set -x
 
 # Check dependecies & install if needed
 if [ ! -z $(command -v faad) ];
@@ -30,6 +30,8 @@ IN_DIR=~/pCleaner-Input
 #OUT_DIR=~/pCleaner-Output
 # Audio Settings
 FX=~/pCleaner-settings
+# Processing History Database
+FILE_DB=~/pCleaner-DB
 
 # Check that IN_DIR, OUT_DIR & FX exist; if not then make them
 if [ ! -e "$IN_DIR" ]; then
@@ -59,37 +61,47 @@ fi
 
 # Send files within the input directory through the audio processing 
 # then loop until the list is finished
-find "$IN_DIR"/ -type f | while IFS=$'\n' read -r INFILE; do
+  find "$IN_DIR"/ -type f ! -name "*.tmp" | while IFS=$'\n' read -r INFILE; do
+
+    # Check if the file has been processed before
+    if fgrep $(md5 -q "$INFILE") "$FILE_DB"; then
+      continue
+    fi
 
     # Check the format of the file, if it is M4A then it will need to be converted due ot a limitation with sox
     # If the file is M4A, it will be converted to WAV using faad and then restart the script
-    INFILE_FORMAT=$(printf "${INFILE##*.}")
+    INFILE_NAME=$(basename "$INFILE")
+    INFILE_FORMAT="${INFILE##*.}"
+
     if [ "$INFILE_FORMAT" = m4a ]; then
       echo "Unsupported format: m4a. File will be converted."
       "$FAAD" -q "$INFILE"
       rsync --remove-source-files "$INFILE" "$IN_DIR"/archive/
       exec "$0"
     fi
-  
-    OUTFILE_NAME=$(printf "$INFILE" | awk -F/ '{print $NF}' | cut -d "." -f 1)
+
+    OUTFILE_PATH=$(dirname "$INFILE")
+      OUTFILE_NAME="${INFILE_NAME%.*}"
   
     # Automatic handling of output formats from a space delimited list
     OUTFILE_FORMAT_LIST='mp3'
     for OUTFILE_FORMAT in $OUTFILE_FORMAT_LIST; do
-      OUTFILE="$OUTFILE_NAME"."$OUTFILE_FORMAT"
-  
+      OUTFILE="$OUTFILE_PATH/$OUTFILE_NAME.$OUTFILE_FORMAT"
+
       echo "$(date -u):"
-    
+
+    cp "$INFILE" "$INFILE".tmp
+
       # This is where the magic happens
       source ~/pCleaner-settings
     
-      "$SOX" -V --no-clobber -t "$INFILE_FORMAT" "$INFILE" --comment "$G" "$OUTFILE" highpass "$HP" lowpass "$LP" mcompand "$AD $K:$T,$R $X1G $F" "$X1" "$AD $K:$T,$R $X1G $F" "$X2" "$AD $K:$T,$R $X2G $F" "$X3" "$AD $K:$T,$R $X3G $F" gain -n "$O"
+      "$SOX" -V -t "$INFILE_FORMAT" "$INFILE".tmp --comment "$G" "$OUTFILE" highpass "$HP" lowpass "$LP" mcompand "$AD $K:$T,$R $X1G $F" "$X1" "$AD $K:$T,$R $X1G $F" "$X2" "$AD $K:$T,$R $X2G $F" "$X3" "$AD $K:$T,$R $X3G $F" gain -n "$O"
     
     done
-  
+
     # Prevent future runs against the same file
-#    rsync --remove-source-files "$INFILE" "$IN_DIR"/archive/
-  
+    echo $(md5 -q "$OUTFILE") "$OUTFILE" | tee -a "$FILE_DB"
+
     if [ -e ./feed-processing.sh ]; then
       ./feed-processing.sh
     fi
